5bd2a03d5d97b6769f6faf387489f1a3b23855d2
Matteo294 <zmatteo.29@gmail.com>
2021-03-23 16:05:06 +0100

    corrected ex2

--- midterm/ex3/higgs.py
+++ midterm/ex3/higgs.py
@@ -16,3 +16,7 @@ def Energy(p, m):
 
 en_Z = Energy(p2, m_e) + Energy(p4, m_e)
 print(en_Z**2 - m_Z**2)
+
+Etot = Energy(p1, m_e) + Energy(p2, m_e) + Energy(p3, m_e) + Energy(p4, m_e) 
+
+print("Higgs boson mass:", Etot)
